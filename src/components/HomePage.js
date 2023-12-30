70e605dae8f22d212288a1b2790bbbddba109b29
Sanjeev Krishna S <sanjeevkrishna2001x@gmail.com>
2023-12-30 11:02:36 +0530

    first commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../components/HomePage.css'
+const HomePage = () => {
+  return (
+    <div className="flexbox-container">
+    
+      <article className="article">
+        <h2>ICT Academy of Kerala</h2>
+        <p>ICT Academy of Kerala (ICTAK) is a social enterprise officially launched on the 24th of June, 2014. The organization had a humble beginning providing skill training programs to selected academic institutions. Over the years, ICTAK has grown to a prime service provider of all ICT and innovation-related training and capacity-building programs in the state.</p>
+      </article>
+
+        <aside className='aside aside1'>
+          
+        
+        </aside>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../components/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <div className="container"> 
+            <div className='logo'> 
+            <h3>ICTAK</h3>
+            </div>
+    <div className='nav-elements'>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/login">Login</Link></li>
+            </ul>
+    </div>
+    </div>
+    </nav>
+  )
+}
+
+export default Navbar
